7c448d9a39c5f6ba24e803e4df3fc27336f19e8d
Gustavo <gustavodiniz37@homail.com>
2024-05-02 10:27:19 -0300

    fix: att deploy settings

--- _todo/settings.py
+++ _todo/settings.py
@@ -60,12 +60,12 @@ CORS_ALLOW_ALL_ORIGINS = (
     True  # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 )
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]  # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    "http://localhost:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+# ]  # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+# CORS_ALLOWED_ORIGIN_REGEXES = [
+#     "http://localhost:3000",
+# ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + MY_APPS
 
